07e95f92a33b6080e999014326d05f50d2e66a4a
LJY <1501926076@qq.com>
2023-02-02 13:34:52 +0800

    单元测试

---
+++ src/FuncTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+
+public class FuncTest {
+
+    @Test
+    public void menu() {
+    }
+
+    @Test
+    public void regester() {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void forget() {
+    }
+
+    @Test
+    public void exit() {
+    }
+}
